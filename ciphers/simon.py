0041d52f72d7279d771d68e676093106262b30c9
Stefan Koelbl <stek@dtu.dk>
2014-12-11 15:54:21 +0100

    refactored simon

--- ciphers/simon.py
+++ ciphers/simon.py
@@ -7,130 +7,139 @@ Created on Mar 28, 2014
 from parser import stpcommands
 from cipher import AbstractCipher
 
-import random
+from parser.stpcommands import getStringLeftRotate as rotl
+from parser.stpcommands import getStringRightRotate as rotr
+
 
 class SimonCipher(AbstractCipher):
- 
+    """
+    Represents the differential behaviour of SIMON and can be used
+    to find differential characteristics for the given parameters.
+    """
+
     def getName(self):
+        """
+        Returns the name of cipher.
+        """
         return "simon"
 
     def getFormatString(self):
-        return ['x', 'y', 'w']
-    
-    def createSTP(self, filename, cipherParameters):
         """
-        Creates an STP file to find a characteristic for Simon with the given parameters.
-        """        
+        Returns the print format.
+        """
+        return ['x', 'y', 'w']
+
+    def createSTP(self, stp_filename, cipherParameters):
+        """
+        Creates an STP stp_file to find a characteristic for Simon with the given
+        parameters.
+        """
+
         wordsize = cipherParameters[0]
-        rotAlpha = cipherParameters[1]
-        rotBeta = cipherParameters[2]
-        rotGamma = cipherParameters[3]
+        rot_alpha = cipherParameters[1]
+        rot_beta = cipherParameters[2]
+        rot_gamma = cipherParameters[3]
         rounds = cipherParameters[4]
         weight = cipherParameters[5]
-        isIterative = cipherParameters[6]
-        varsFixed = cipherParameters[7]
-        blockedCharacteristics = cipherParameters[8]
-        
-        with open(filename, 'w') as file:
-            file.write("% Input File for STP\n% Simon w={} alpha={} beta={} gamma={} rounds={}\n\n\n".format(wordsize, rotAlpha, rotBeta, rotGamma, rounds))
-               
-            # Setup variable
+        is_iterative = cipherParameters[6]
+        fixed_vars = cipherParameters[7]
+        chars_blocked = cipherParameters[8]
+
+        with open(stp_filename, 'w') as stp_file:
+            stp_file.write("% Input File for STP\n% Simon w={} alpha={} beta={} gamma={}"
+                       " rounds={}\n\n\n".format(wordsize, rot_alpha,
+                                                 rot_beta, rot_gamma, rounds))
+
+            # Setup variables
             # x = left, y = right
             x = ["x{}".format(i) for i in range(rounds + 1)]
             y = ["y{}".format(i) for i in range(rounds + 1)]
             and_out = ["andout{}".format(i) for i in range(rounds + 1)]
-            
+
             # w = weight
             w = ["w{}".format(i) for i in range(rounds)]
-            
-            stpcommands.setupVariables(file, x, wordsize)
-            stpcommands.setupVariables(file, y, wordsize)
-            stpcommands.setupVariables(file, and_out, wordsize)
-            stpcommands.setupVariables(file, w, wordsize)
-            
-            stpcommands.setupWeightComputation(file, weight, w, wordsize)
-               
+
+            stpcommands.setupVariables(stp_file, x, wordsize)
+            stpcommands.setupVariables(stp_file, y, wordsize)
+            stpcommands.setupVariables(stp_file, and_out, wordsize)
+            stpcommands.setupVariables(stp_file, w, wordsize)
+
+            stpcommands.setupWeightComputation(stp_file, weight, w, wordsize)
+
             for i in range(rounds):
-                self.setupSimonRound(file, x[i], y[i], x[i+1], y[i+1], and_out[i], w[i], rotAlpha, rotBeta, rotGamma, wordsize)
-        
+                self.setupSimonRound(stp_file, x[i], y[i], x[i+1], y[i+1], and_out[i],
+                                     w[i], rot_alpha, rot_beta, rot_gamma, wordsize)
+
             # No all zero characteristic
-            stpcommands.assertNonZero(file, x + y, wordsize)
-            
+            stpcommands.assertNonZero(stp_file, x + y, wordsize)
+
             # Iterative characteristics only
             # Input difference = Output difference
-            if(isIterative):
-                stpcommands.assertVariableValue(file, x[0], x[rounds])
-                stpcommands.assertVariableValue(file, y[0], y[rounds])
-                
-            if(varsFixed):
-                for key, value in varsFixed.iteritems():
-                    stpcommands.assertVariableValue(file, key, value)
-                    
-            if(blockedCharacteristics):
-                for char in blockedCharacteristics:
-                    stpcommands.blockCharacteristic(file, char, wordsize)
-            
-            stpcommands.setupQuery(file)
+            if is_iterative:
+                stpcommands.assertVariableValue(stp_file, x[0], x[rounds])
+                stpcommands.assertVariableValue(stp_file, y[0], y[rounds])
+
+            if fixed_vars:
+                for key, value in fixed_vars.iteritems():
+                    stpcommands.assertVariableValue(stp_file, key, value)
+
+            if chars_blocked:
+                for char in chars_blocked:
+                    stpcommands.blockCharacteristic(stp_file, char, wordsize)
+
+            stpcommands.setupQuery(stp_file)
 
         return
-    
+
     def getParamList(self, rounds, wordsize, weight):
         """
         TODO:
         """
         return [wordsize, 1, 8, 2, rounds, weight]
-    
-        
-    def setupSimonRound(self, file, x_in, y_in, x_out, y_out, and_out, w, rotAlpha, rotBeta, rotGamma, wordsize):
-        file.write(self.getStringForSimonRound(x_in, y_in, x_out, y_out, and_out, w, rotAlpha, rotBeta, rotGamma, wordsize) + '\n')
-        return
-    
-    def getDependentBitsForAND(self, x_in, wordsize):
-        "rotate_right(diff, 6) & (~rotate_left(diff, 1)) & rotate_left(diff, 8);"
-        command = "({0} & (~{1}) & {2})".format(stpcommands.getStringRightRotate(x_in, 6, wordsize),
-                                              stpcommands.getStringLeftRotate(x_in, 1, wordsize),
-                                              stpcommands.getStringLeftRotate(x_in, 8, wordsize));
-        return command
-    
-    def getStringForSimonRound(self, x_in, y_in, x_out, y_out, and_out, w, rotAlpha, rotBeta, rotGamma, wordsize):
+
+    def setupSimonRound(self, stp_file, x_in, y_in, x_out, y_out, and_out, w,
+                        rot_alpha, rot_beta, rot_gamma, wordsize):
         """
-        Returns a string representing one round of Simon in STP.
-        
+        Model for differential behaviour of one round SIMON
         y[i+1] = x[i]
-        x[i] = (x[i] <<< 1) & (x[i] <<< 8) ^ y[i] ^ (x[i] << 2) 
+        x[i+1] = (x[i] <<< 1) & (x[i] <<< 8) ^ y[i] ^ (x[i] << 2)
         """
         command = ""
-        
+
         #Assert(y_out = x_in)
         command += "ASSERT({} = {});\n".format(y_out, x_in)
-        
-        #Assert AND Output
-        command += "ASSERT(" + stpcommands.getStringForAndDifferential(stpcommands.getStringLeftRotate(x_in, rotAlpha, wordsize),
-                                                            stpcommands.getStringLeftRotate(x_in, rotBeta, wordsize),
-                                                            and_out) + " = 0hex{});\n".format("f"*(wordsize / 4))
-                  
-                  
-        #Deal with dependent inputs
-        dependentRotated = stpcommands.getStringLeftRotate(self.getDependentBitsForAND(x_in, wordsize),7,wordsize)
-        andoutRotatedMasked = stpcommands.getStringRightRotate("({0} & {1})".format(and_out,dependentRotated), 7, wordsize)
-        
-        command += "ASSERT(BVXOR({0} & {1}, {2}) = 0hex{3});\n".format(and_out, self.getDependentBitsForAND(x_in, wordsize), andoutRotatedMasked, "0"*(wordsize / 4))          
-                                                            
-        #Assert XORs
-        command += "ASSERT(" + x_out + " = "
-        command += "BVXOR("
-        command += stpcommands.getStringLeftRotate(x_in, rotGamma, wordsize)
-        command += ", BVXOR(" + y_in + ", " + and_out + ")"
-        command += "));\n"
-                                                            
-        #More improved :)
-        command += "ASSERT({0} = (IF {2} = 0x{4} THEN BVSUB({5},0x{4},0x{6}1) ELSE BVXOR(~{1} & ~{2} & {3}, {2}) ENDIF));".format(w, stpcommands.getStringRightRotate(x_in, 7, wordsize),
-                                                                          x_in, stpcommands.getStringRightRotate(x_in, 14, wordsize),
-                                                                          "f"*(wordsize / 4), wordsize, "0"*((wordsize / 4)- 1));
-        
-                                                                  
-                
-        return command
 
-    
-    
+        x_in_rotalpha = rotl(x_in, rot_alpha, wordsize)
+        x_in_rotbeta = rotl(x_in, rot_beta, wordsize)
+
+        #Assert AND Output
+        command += "ASSERT({} = 0hex{});\n".format(
+            stpcommands.getStringForAndDifferential(x_in_rotalpha, x_in_rotbeta,
+                                                    and_out), "f"*(wordsize / 4))
+
+        #Deal with dependent inputs
+        rot_dependent = rotl(self.getDependentBitsForAND(x_in, wordsize), 7, wordsize)
+        andout_rotmask = rotr("({0} & {1})".format(and_out, rot_dependent), 7, wordsize)
+
+        command += "ASSERT(BVXOR({0} & {1}, {2}) = 0hex{3});\n".format(
+            and_out, self.getDependentBitsForAND(x_in, wordsize),
+            andout_rotmask, "0"*(wordsize / 4))
+
+        #Assert XORs
+        command += "ASSERT({} = BVXOR({}, BVXOR({}, {})));\n".format(
+            x_out, rotl(x_in, rot_gamma, wordsize), y_in, and_out)
+
+        #Weight computation
+        command += "ASSERT({0} = (IF {2} = 0x{4} THEN BVSUB({5},0x{4},0x{6}1) \
+                   ELSE BVXOR(~{1} & ~{2} & {3}, {2}) ENDIF));".format(
+                   w, rotr(x_in, 7, wordsize), x_in, rotr(x_in, 14, wordsize),
+                   "f"*(wordsize / 4), wordsize, "0"*((wordsize / 4) - 1))
+        stp_file.write(command)
+        return
+
+    def getDependentBitsForAND(self, x_in, wordsize):
+        "rotate_right(diff, 6) & (~rotate_left(diff, 1)) & rotate_left(diff, 8);"
+        command = "({0} & (~{1}) & {2})".format(rotr(x_in, 6, wordsize),
+                                                rotl(x_in, 1, wordsize),
+                                                rotl(x_in, 8, wordsize))
+        return command
